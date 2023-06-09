1fd7cb121ffe8ec2fbd6eb46b4ad8adbc367e3d1
Richmond Gyekye <mrgyekye740@gmail.com>
2023-06-09 11:27:52 +0000

    Initial Commit

---
+++ nollab/nollabApp.swift
@@ -0,0 +1,17 @@
+//
+//  nollabApp.swift
+//  nollab
+//
+//  Created by richmond gyekye on 09/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct nollabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
