1b69d7debe9e965a1d4a6d09e9015d4cb86f90ad
Richmond Gyekye <mrgyekye740@gmail.com>
2023-06-11 10:48:00 +0000

    feat(Firebase): Add firebase to project

--- nollab/nollabApp.swift
+++ nollab/nollabApp.swift
@@ -1,14 +1,20 @@
-//
-//  nollabApp.swift
-//  nollab
-//
-//  Created by richmond gyekye on 09/06/2023.
-//
 
 import SwiftUI
+import FirebaseCore
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
 
 @main
 struct nollabApp: App {
+    // Register firebase app here
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     var body: some Scene {
         WindowGroup {
             ContentView()
